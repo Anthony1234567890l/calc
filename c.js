cdecf75386ab3b0d5ec3d13b4917b659bdc26dea
Anthony1234567890l <134167391+Anthony1234567890l@users.noreply.github.com>
2024-05-05 12:38:26 -0400

    Add files via upload

---
+++ c.js
@@ -0,0 +1,41 @@
+const d1 = 0
+const x1 = document.querySelectorAll("#digits-1, #digits-2, #digits-3, #digits-4, #digits-5, #digits-6, #digits-7, #digits-8, #digits-9, #digits-0");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1 ;
+
+const dopteror = 0
+const x1 = document.querySelectorAll("#plus, #multiply, #minus, ");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1 ;
+
+
+/*
+const d2 = 0
+const x1 = document.querySelector("#digits-2");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1;
+
+const d3 = 0
+const x1 = document.querySelector("#digits-3");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1;
+
+const d4 = 0
+const x1 = document.querySelector("#digits-4");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1;
+
+const d5 = 0
+const x1 = document.querySelector("#digits-5");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1;
+
+const d6 = 0
+const x1 = document.querySelector("#digits-6");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1;
+
+const d7 = 0
+const x1 = document.querySelector("#digits-7");
+x1.onclick = () => alert(1);
+x1.onclick = () => d1 = 1; */

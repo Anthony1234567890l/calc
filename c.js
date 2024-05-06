b1592255a7840041a4f5c59af41219608533d7a3
Anthony1234567890l <134167391+Anthony1234567890l@users.noreply.github.com>
2024-05-06 09:32:37 -0400

    Update c.js

--- c.js
+++ c.js
@@ -8,34 +8,10 @@ const x1 = document.querySelectorAll("#plus, #multiply, #minus, ");
 x1.onclick = () => alert(1);
 x1.onclick = () => d1 = 1 ;
 
+var arr = ["#digits-0", "#digits-1", "#digits-2", "#digits-3", "#digits-4", "#digits-5", "#digits-6", "#digits-7", "#digits-8", "#digits-9",];
+for(var i = 0; i <= 9; i++){
+  arr[i].onclick = () => alert(i)
+}
 
-/*
-const d2 = 0
-const x1 = document.querySelector("#digits-2");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1;
 
-const d3 = 0
-const x1 = document.querySelector("#digits-3");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1;
 
-const d4 = 0
-const x1 = document.querySelector("#digits-4");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1;
-
-const d5 = 0
-const x1 = document.querySelector("#digits-5");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1;
-
-const d6 = 0
-const x1 = document.querySelector("#digits-6");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1;
-
-const d7 = 0
-const x1 = document.querySelector("#digits-7");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1; */

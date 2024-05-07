90f7a63724e9a892b18e53ca9c369c7cc66530f5
Anthony1234567890l <134167391+Anthony1234567890l@users.noreply.github.com>
2024-05-07 19:49:48 -0400

    Update c.js

--- c.js
+++ c.js
@@ -1,17 +1,18 @@
-const d1 = 0
-const x1 = document.querySelectorAll("#digits-1, #digits-2, #digits-3, #digits-4, #digits-5, #digits-6, #digits-7, #digits-8, #digits-9, #digits-0");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1 ;
-
-const dopteror = 0
-const x1 = document.querySelectorAll("#plus, #multiply, #minus, ");
-x1.onclick = () => alert(1);
-x1.onclick = () => d1 = 1 ;
 
 var arr = ["#digits-0", "#digits-1", "#digits-2", "#digits-3", "#digits-4", "#digits-5", "#digits-6", "#digits-7", "#digits-8", "#digits-9",];
 for(var i = 0; i <= 9; i++){
   arr[i].onclick = () => alert(i)
 }
 
+const x1 = arr
+var arr["#plus", "#minus", "#multiply", "#divide", ];
+for( var i2 =  +; var i2 == -; var i2 == x; var i2 == /){
+  arr[i2].onclick = () => alert(i2)
+} 
 
+var arr2 = ["#digits-0", "#digits-1", "#digits-2", "#digits-3", "#digits-4", "#digits-5", "#digits-6", "#digits-7", "#digits-8", "#digits-9",];
+for(var i3 = 0; i <= 9; i3++){
+  arr[i].onclick = () => alert(i)
+}
 
+const x2 = arr2
